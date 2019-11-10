0944e54a37707ce39250c9aac4578b5e302d1b4c
laboon <laboon@gmail.com>
2019-11-10 16:11:21 +0100

    formatting; add some tests; update twiddle

--- basic_hash/src/main.rs
+++ basic_hash/src/main.rs
@@ -2,7 +2,7 @@
 use std::env;
 
 const BLOCK_SIZE: usize = 8;
-const INITIALIZATION_VECTOR: u64 = 0x0123456789ABCDEF;
+const INITIALIZATION_VECTOR: u64 = 0x0123_4567_89AB_CDEF;
 
 fn print_usage_and_exit() {
     println!("Please enter one and only one argument");
@@ -32,18 +32,33 @@ fn get_to_hash() -> Vec<u8> {
 
 fn strengthen(data: Vec<u8>) -> Vec<u8> {
     let rem = data.len() % BLOCK_SIZE;
-    if rem == 0 {
+    if rem == 0 && data.len() > 0 {
         // do nothing, no padding necessary
-        return data;
+        data
     } else {
-        let mut to_return = data.clone();
+        let mut to_return = data;
         let n = BLOCK_SIZE - rem;
         for _j in 0..n {
             to_return.push(0);
         }
-        return to_return;
+        to_return
     }
 }
+
+fn twiddle(arr: &mut [u8; BLOCK_SIZE]) {
+
+    for j in 0..BLOCK_SIZE {
+        arr[j % BLOCK_SIZE] ^=
+            arr[(j + 1) % BLOCK_SIZE] << ((j + 7) % BLOCK_SIZE)
+            ^ arr[(j + 2) % BLOCK_SIZE] << ((j + 6) % BLOCK_SIZE)
+            ^ arr[(j + 3) % BLOCK_SIZE] << ((j + 5) % BLOCK_SIZE)
+            ^ arr[(j + 4) % BLOCK_SIZE] << ((j + 4) % BLOCK_SIZE)
+            ^ arr[(j + 5) % BLOCK_SIZE] >> ((j + 3) % BLOCK_SIZE)
+            ^ arr[(j + 6) % BLOCK_SIZE] >> ((j + 2) % BLOCK_SIZE)
+            ^ arr[(j + 7) % BLOCK_SIZE] >> ((j + 1) % BLOCK_SIZE);
+    }
+}
+
 
 fn transform(cv: u64, arr: [u8; BLOCK_SIZE]) -> u64 {
     let mut to_return: [u8; BLOCK_SIZE] = [0; BLOCK_SIZE];
@@ -54,7 +69,7 @@ fn transform(cv: u64, arr: [u8; BLOCK_SIZE]) -> u64 {
     }
 
     for j in 0..1024 {
-        to_return[j % BLOCK_SIZE] ^= to_return[(j + 1) % BLOCK_SIZE] << 1 ^ to_return[(j + 2) % BLOCK_SIZE] >> 1 ^ to_return[(j + 3) % BLOCK_SIZE] << 2 ^ to_return[(j + 4) % BLOCK_SIZE] >> 2;
+        twiddle(&mut to_return);
     }
 
     u64::from_le_bytes(to_return)
@@ -62,7 +77,7 @@ fn transform(cv: u64, arr: [u8; BLOCK_SIZE]) -> u64 {
 }
 
 fn compress(cv: u64, data: Vec<u8>) -> u64 {
-    let mut new_data = data.clone();
+    let mut new_data = data;
     let mut a: [u8; BLOCK_SIZE] = [0; BLOCK_SIZE];
     for j in 0..BLOCK_SIZE {
         a[j] = new_data.pop().unwrap();
@@ -89,30 +104,149 @@ fn split(data: Vec<u8>) -> Vec<Vec<u8>> {
 }
 
 fn finalize(to_finalize: u64) -> u64 {
-    to_finalize ^ 0xFFFFFFFF
+    to_finalize ^ 0xFFFF_FFFF_FFFF_FFFF
 }
 
 fn bill_hash(h: Vec<u8>) -> u64 {
     let mut cv: u64 = INITIALIZATION_VECTOR;
     let blocks = split(h);
-    println!("cv = {} (IV)", cv);
     for block in blocks {
         cv = compress(cv, block);
-        println!("cv = {}", cv);
     }
 
     finalize(cv)
 }
 
 
+
 fn main() {
 
-    let to_hash = get_to_hash();
-
-    println!("{:?}", to_hash);
-
-    let hash_val = bill_hash(to_hash);
-
+    let hash_val = bill_hash(get_to_hash());
     println!("Hash value: {:#016x}", hash_val);
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // strengthen
+
+    #[test]
+    fn test_strengthen_empty_arr() {
+        let to_test = Vec::new();
+        let expected = [0, 0, 0, 0, 0, 0, 0, 0];
+        assert_eq!(strengthen(to_test), expected);
+    }
+
+    #[test]
+    fn test_strengthen_one_elem_arr() {
+        let to_test = vec![1];
+        let expected = [1, 0, 0, 0, 0, 0, 0, 0];
+        assert_eq!(strengthen(to_test), expected);
+    }
+
+    #[test]
+    fn test_strengthen_four_elem_arr() {
+        let to_test = vec![0, 1, 2, 3];
+        let expected = [0, 1, 2, 3, 0, 0, 0, 0];
+        assert_eq!(strengthen(to_test), expected);
+    }
+
+    #[test]
+    fn test_strengthen_eight_elem_arr() {
+        let to_test = vec![0, 1, 2, 3, 4, 5, 6, 7];
+        let expected = [0, 1, 2, 3, 4, 5, 6, 7];
+        assert_eq!(strengthen(to_test), expected);
+    }
+
+    // split
+
+    #[test]
+    fn test_split_empty_arr() {
+        let to_test = Vec::new();
+        let expected = [[0, 0, 0, 0, 0, 0, 0, 0]];
+        assert_eq!(split(to_test), expected);
+    }
+
+    #[test]
+    fn test_split_single_elem() {
+        let to_test = vec![1];
+        let expected = [[1, 0, 0, 0, 0, 0, 0, 0]];
+        assert_eq!(split(to_test), expected);
+    }
+
+    #[test]
+    fn test_split_same_as_block_size() {
+        let to_test = vec![0, 1, 2, 3, 4, 5, 6, 7];
+        let expected = [[0, 1, 2, 3, 4, 5, 6, 7]];
+        assert_eq!(split(to_test), expected);
+    }
+
+    #[test]
+    fn test_split_one_more_than_block_size() {
+        let to_test = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let expected = [[0, 1, 2, 3, 4, 5, 6, 7],
+                        [8, 0, 0, 0, 0, 0, 0, 0]];
+        assert_eq!(split(to_test), expected);
+    }
+
+    #[test]
+    fn test_split_several_blocks_no_padding() {
+        let to_test = vec![0, 1, 2, 3, 4, 5, 6, 7, 8,
+        9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3];
+        let expected = [[0, 1, 2, 3, 4, 5, 6, 7],
+                        [8, 9, 0, 1, 2, 3, 4, 5],
+                        [6, 7, 8, 9, 0, 1, 2, 3]];
+        assert_eq!(split(to_test), expected);
+    }
+
+
+    #[test]
+    fn test_split_several_blocks_with_padding() {
+        let to_test = vec![0, 1, 2, 3, 4, 5, 6, 7, 8,
+        9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let expected = [[0, 1, 2, 3, 4, 5, 6, 7],
+                        [8, 9, 0, 1, 2, 3, 4, 5],
+                        [6, 7, 8, 9, 0, 0, 0, 0]];
+        assert_eq!(split(to_test), expected);
+    }
+
+    // transform
+
+    #[test]
+    fn test_transform_iv_0() {
+        let cv = INITIALIZATION_VECTOR;
+        let to_test = [0, 1, 2, 3, 4, 5, 6, 7];
+        // assert_eq!(transform(cv, to_test), 15536890553721539187);
+
+    }
+
+
+    // compress
+
+    // finalize
+
+    #[test]
+    fn test_finalize_0() {
+        assert_eq!(finalize(0), 0xFFFFFFFFFFFFFFFF);
+    }
+
+    #[test]
+    fn test_finalize_maxint() {
+        assert_eq!(finalize(0xFFFFFFFFFFFFFFFF), 0);
+    }
+
+    #[test]
+    fn test_finalize_mixed() {
+        assert_eq!(finalize(0xF0F0F0F0F0F0F0F0), 0x0F0F0F0F0F0F0F0F);
+    }
+
+    #[test]
+    fn test_finalize_some_value() {
+        assert_eq!(finalize(3987120094), 18446744069722431521);
+    }
+
+    // bill_hash
 
 }
